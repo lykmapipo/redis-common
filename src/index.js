3251afa1c24c983612a255f65cd499998fc2eab0
lykmapipo <lallyelias87@gmail.com>
2019-06-25 14:54:01 +0300

    chore(setup): initialize

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {
+  forEach
+} from 'lodash';
+
+/**
+ * @function createClients
+ * @name createClients
+ * @description Convert list of values to upper values
+ * @param {String[]|...String} values list to convert to upper
+ * @return {String[]} list of upper values
+ * @author lally elias <lallyelias87@gmail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const { client, publisher, subscriber } = createClients();
+ * 
+ * const { client, publisher, subscriber } = createClients({ new: true });
+ * 
+ */
+export const createClients = (optns = {}) => {};
---
+++ test/index.spec.js
@@ -0,0 +1,6 @@
+import { expect } from '@lykmapipo/test-helpers';
+import {
+  createClients
+} from '../src/index';
+
+describe('redis-common', () => {});
