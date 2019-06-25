5d8de4d0f5f257cf5a02ebec59a5c4b1d2287e76
lykmapipo <lallyelias87@gmail.com>
2019-06-25 15:10:52 +0300

    chore(setup): add common dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import {
-  forEach
-} from 'lodash';
+import { compact, mergeObjects } from '@lykmapipo/common';
+import { getString } from '@lykmapipo/env';
+
+/**
+ * @function withDefaults
+ * @name withDefaults
+ * @description merge provided options with defaults.
+ * @param {Object} [optns] provided options
+ * @return {Object} merged options
+ * @author lally elias <lallyelias87@gmail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * const { withDefaults } = require('@lykmapipo/keu-common');
+ * const redis = (process.env.REDIS_URL || { port: 6379, host: '127.0.0.1' });
+ * const options = withDefaults({ redis })
+ *
+ * // => { redis: ...}
+ *
+ */
+export const withDefaults = optns => {
+  // defaults
+  const defaults = {
+    redis: getString('REDIS_URL') || { port: 6379, host: '127.0.0.1' },
+  };
+
+  // merge and compact with defaults
+  const options = compact(mergeObjects(defaults, optns));
+
+  // return
+  return options;
+};
 
 /**
  * @function createClients
  * @name createClients
- * @description Convert list of values to upper values
- * @param {String[]|...String} values list to convert to upper
- * @return {String[]} list of upper values
+ * @description Create redis clients
+ * @param {Object} optns valid options
+ * @return {Object} redis clients
  * @author lally elias <lallyelias87@gmail.com>
  * @license MIT
  * @since 0.1.0
@@ -17,8 +50,8 @@ import {
  * @example
  *
  * const { client, publisher, subscriber } = createClients();
- * 
+ *
  * const { client, publisher, subscriber } = createClients({ new: true });
- * 
+ *
  */
-export const createClients = (optns = {}) => {};
+export const createClients = () => {};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,6 @@
 import { expect } from '@lykmapipo/test-helpers';
-import {
-  createClients
-} from '../src/index';
+import { withDefaults } from '../src/index';
 
-describe('redis-common', () => {});
+describe('redis-common', () => {
+  expect(withDefaults).to.exist;
+});
